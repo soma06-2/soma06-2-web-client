89019140a89db5a128a9f9ec547b41cd8a5b6cc4
beingbook@gmail.com <beingbook@gmail.com>
2015-11-13 09:10:03 +0900

    Add space background to HomePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,12 +6,35 @@ import styles from './HomePage.css';
 import withStyles from '../../decorators/withStyles';
 // import Button from 'react-toolbox/lib/button';
 
+@withStyles(styles)
 class HomePage extends Component {
 
   render() {
     return (
-      <div className="wrap">
-        <p>뭔가 검색해줘요</p>
+      <div>
+        <div className="wrap HomePage-text">
+          <div
+            style={{
+              padding: '60px 0px',
+            }}>
+            <h1>
+              Review Summarizer
+              <span
+                style={{
+                  fontStyle: 'italic',
+                  lineHeight: '2',
+                  display: 'block',
+                  fontSize: '0.4em',
+                }}>
+                Software Maestro 6th, Stage 1, Step 2
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="HomePage-banner">
+          <img src={require('./space.jpg')} alt="" className="image1" />
+          <img src={require('./space.jpg')} alt="" className="image2" />
+        </div>
       </div>
     );
   }
